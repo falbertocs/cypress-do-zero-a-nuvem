1597bcfdbf1a9d9b422e39744f8c505c4f2cb1b8
Fabio Cruz <39712687+falbertocs@users.noreply.github.com>
2025-09-05 17:48:51 -0300

    Configure the ci workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,14 @@
-const { defineConfig } = require('Cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implementar event listeners aqui
+    },
+  },
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
+  projectId: 'yam3vm',
 })
